f16950ea3960852b49a55875d078fb5108fd9f81
shubham-Gund-21 <angulardeveloper239@gmail.com>
2024-12-14 21:11:38 +0530

    variable,comment,syntax

---
+++ 14-12-24/Demo.java
@@ -0,0 +1,21 @@
+public class Demo{
+    public static void main(String... args){
+        // System.out.println("Hello, World!");
+        // System.out.println(args[0]); 
+    int num1=500;
+    int num2=100;
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println(num1+num2);
+    //Addition of two numbers single line comment
+    /*
+    This is a multi-line comment
+    */
+
+    System.out.println("Addition of two numbers "+(num1)+" + "+(num2)+" = "+(num1+num2));
+
+
+    
+    }
+
+}
